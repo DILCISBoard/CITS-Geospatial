8914ef921bcc9e8a0260ca191b367d9290a787ef
Carl Wilson <carl@openpreservation.org>
2024-04-18 22:20:43 +0100

    FEAT: Site publication workflow.

--- create_site.sh
+++ create_site.sh
@@ -7,18 +7,18 @@ echo " - Cleaning up site directory and copying spec-publisher site..."
 git clean -f site/ specification/
 cp -rf spec-publisher/site/* site/
 
-echo " - Generating main site page..."
+echo " - Generating main site specification and PDF markdown..."
 
+mvn clean package -f spec-publisher/pom.xml
+java -jar ./spec-publisher/target/mets-profile-processor-0.1.0-SNAPSHOT.jar -f ./index.yaml -o doc/site profile/E-ARK-GEOSPATIAL-REPRESENTATION.xml profile/E-ARK-GEOSPATIAL-ROOT.xml
+
+echo " - MARKDOWN-PP: generating site page with TOC..."
+cd doc/site || exit
 bash "$SCRIPT_DIR/spec-publisher/scripts/create-venv.sh"
-
 command -v markdown-pp >/dev/null 2>&1 || {
   tmpdir=$(dirname "$(mktemp -u)")
   source "$tmpdir/.venv-markdown/bin/activate"
 }
-
-cd doc/site || exit
-
-echo " - MARKDOWN-PP: generating site page with TOC..."
 markdown-pp body.md -o body_toc.md
 
 echo " - MARKDOWN-PP: generating site index.md..."
@@ -39,4 +39,3 @@ then
   rm -rf _site
 fi
 mkdir _site
-docker run --rm -v "$PWD"/site:/usr/src/app -v "$PWD"/_site:/_site starefossen/github-pages jekyll build -d /_site
